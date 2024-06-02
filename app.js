4f7a34f9766ce22bd337bfc7f5882d2ff9a64f8b
JM Maglonzo <johnmaglonzo@gmail.com>
2024-06-02 09:42:46 +0800

    add cors

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ import express from "express";
 import morgan from "morgan";
 import workoutRoutes from "./routes/workoutRouter.js";
 import errorHandler from "./middleware/errorHandler.js";
+import cors from "cors";
 const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use("/api/workouts", workoutRoutes);
 app.use(errorHandler);
 export default app;
