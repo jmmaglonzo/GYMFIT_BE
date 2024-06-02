735d3c11a24c73777bc0a542b0c4a4a8c3f0d365
JM Maglonzo <johnmaglonzo@gmail.com>
2024-06-02 09:24:44 +0800

    change path

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import morgan from "morgan";
-import workoutRoutes from "./api/routes/workoutRouter.js";
-import errorHandler from "./api/middleware/errorHandler.js";
+import workoutRoutes from "./routes/workoutRouter.js";
+import errorHandler from "./middleware/errorHandler.js";
 const app = express();
 
 app.use(express.json());
